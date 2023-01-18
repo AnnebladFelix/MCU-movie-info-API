87a7ce70d56958129cdf7c21c0991ea2153587de
Felix Anneblad <felix.anneblad@gmail.com>
2023-01-18 19:52:01 +0100

    First commit

---
+++ mcuapp.ts
@@ -0,0 +1,57 @@
+let MCUEl = document.getElementById('mcu-app')!;
+let nxtBtn = document.getElementById('nxt-btn')!;
+let preBtn = document.getElementById('pre-btn')!;
+const url = "https://mcuapi.herokuapp.com/api/v1/movies";
+const p = fetch(url)
+.then(response => {
+    return response.json();
+}).then(marvel => {
+    let i = -1;
+
+    nxtBtn.addEventListener('click', function(e){
+        e.preventDefault();
+        MCUEl.innerHTML = "";
+        i++;
+        foo();
+    })
+
+    preBtn.addEventListener('click', function(e){
+        e.preventDefault();
+        MCUEl.innerHTML = "";
+        i--;
+        foo();
+
+    })
+
+    //for(let i = 0; i < marvel.data.length; i++){
+    const foo = function(){
+        let div1 = document.createElement('div') as HTMLElement;
+        let info = document.createElement('p') as HTMLParagraphElement;
+        let release = document.createElement('p') as HTMLParagraphElement;
+        let img = document.createElement("img") as HTMLImageElement;
+        let title = document.createElement('h2') as HTMLElement;
+
+         MCUEl.append(div1);
+
+         div1.className = 'card';
+        //  info.className = 'info hidden';
+        //  img.className = 'cover-img';
+        //  title.className = 'title hidden';
+        //  release.className = 'release hidden';
+
+         info.innerHTML = marvel.data[i].overview;
+         title.innerHTML = marvel.data[i].title;
+         release.innerHTML = marvel.data[i].release_date;
+         img.innerHTML = marvel.data[i].cover_url;
+         div1.append(title, info, img, release)
+         div1.innerHTML = (`Title: ${marvel.data[i].title} <br /> 
+         info: ${marvel.data[i].overview} <br /> 
+         release date: ${marvel.data[i].release_date}<br /> 
+         <img src="${marvel.data[i].cover_url}" class="cover-img">`);
+    }
+    // MCUEl.innerHTML = `Title: ${marvel.data[0].title} <br /> 
+    // info: ${marvel.data[0].overview} <br /> 
+    // release date: ${marvel.data[0].release_date} <br /> 
+    // <img src="${marvel.data[0].cover_url}" class="cover-img">
+    // `
+})
